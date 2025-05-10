956e7d43dafcea822a9692a1bfe696caac42863c
Aldair Lara <aldairlara@gmail.com>
2025-05-09 23:06:30 -0600

    Creación del proyecto c24

---
+++ c24-GPTM_EdgeCount/main.c
@@ -0,0 +1,37 @@
+/*********************************************************************************
+ * Universidad Nacional Autónoma de México (UNAM)
+ * Facultad de Ingeniería | Departamento de Electrónica
+ * 
+ * Asignatura:  Microprocesadores y Microcontroladores
+ * Profesor:    M.I. Christo Aldair Lara Tenorio
+ * Fecha:       09 de mayo de 2025
+ * 
+ * Tema 09:     Periféricos
+ * Código 24:   GPTM: Modo Edge-Count
+ * Descripción: XXX.
+ *              Periféricos utilizados:
+ * 
+ * Tarjeta de desarrollo:	EK-TM4C1294XL Evaluation board
+ *********************************************************************************/
+
+
+/*********************************************************************************
+ * Archivos de cabecera
+ */
+
+#include <stdint.h>                                                                 /*  Definición de los tamaños de tipo entero */
+
+
+/*********************************************************************************
+ * Variables globales
+ */
+
+
+/*********************************************************************************
+ * Función principal
+ */
+
+int main(void) {
+
+    while(1) {}
+}
