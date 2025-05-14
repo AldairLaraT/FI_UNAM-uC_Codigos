0c348b0d5a2ba042520cfc1f65146b1f6f08bf33
Aldair Lara <aldairlara@gmail.com>
2025-05-14 23:29:28 +0900

    NVIC.c

--- c24-GPTM_EdgeCount/NVIC.c
+++ c24-GPTM_EdgeCount/NVIC.c
@@ -22,93 +22,22 @@
 
 
 /*********************************************************************************
- * Variables globales
- */
-
-extern uint16_t ReloadStep;
-extern uint32_t TIMER2_TimeoutValue;
-extern float ReactionTime_ms;
-
-
-/*********************************************************************************
  * Funciones
  */
 
-void TIMER1_A_Handler(void){
+void TIMER0_A_Handler(void){
 
-    GPTM1_A_ClearFlags_TimeOut();                                                   /*  GPTM1 => TATOCINT: GPTM Timer A Time-Out Raw Interrupt -> TATORIS bit (GPTMRIS) and TATOMIS bit (GPTMMIS) cleared */
+    GPTM0_A_ClearFlags_EdgeCount();                                                 /*  GPTM0 => CAMCINT: GPTM Timer A Capture Mode Match Interrupt Clear -> CAMRIS bit (GPTMRIS) and CAMMIS bit (GPTMMIS) cleared */
 
-    LED_D1_Off();
+    LED_D1_Toggle();
 
-    TIMER2_TAILR_R = (TIMER2_TimeoutValue);                                         /*  GPTM2 => TAILR: GPTM Timer A Interval Load */
-    GPTM2_A_Initiate();                                                             /*  GPTM2 => TAEN: GPTM Timer A Enable -> Enabled and begins counting */
 }
 
 
-void TIMER2_A_Handler(void){
+void TIMER0_B_Handler(void){
 
-    GPTM2_A_ClearFlags_TimeOut();                                                   /*  GPTM2 => TATOCINT: GPTM Timer A Time-Out Raw Interrupt -> TATORIS bit (GPTMRIS) and TATOMIS bit (GPTMMIS) cleared */
+    GPTM0_B_ClearFlags_EdgeCount();                                                 /*  GPTM0 => CBMCINT: GPTM Timer B Capture Mode Match Interrupt Clear -> CBMRIS bit (GPTMRIS) and CBMMIS bit (GPTMMIS) cleared */
 
-    LED_D4_On();
+    LED_D2_Toggle();
 
-    NVIC_DIS0_R |= (1 << (21 - 0));                                                 /*  Interrupt 21 (TIMER 1 subtimer A) => INT: Interrupt Disable -> INT bit (EN0) cleared */
-    NVIC_DIS0_R |= (1 << (23 - 0));                                                 /*  Interrupt 23 (TIMER 2 subtimer A) => INT: Interrupt Disable -> INT bit (EN0) cleared */
-    NVIC_DIS1_R |= (1 << (51 - 32));                                                /*  Interrupt 51 (PortJ) => INT: Interrupt Disable -> INT bit (EN1) cleared */
-}
-
-
-void GPIO_PortJ_Handler(void){
-
-    /*  Retardo de rebote */
-    GPTM0_A_Initiate();                                                             /*  GPTM0 => TAEN: GPTM Timer A Enable -> Enabled and begins counting */
-    GPTM0_A_RawInterrupt_TimeOut_wait();                                            /*  GPTM0 => TATORIS: GPTM Timer A Time-Out Raw Interrupt -> Timer A has timed out */
-    GPTM0_A_ClearFlags_TimeOut();                                                   /*  GPTM0 => TATOCINT: GPTM Timer A Time-Out Raw Interrupt -> TATORIS bit (GPTMRIS) and TATOMIS bit (GPTMMIS) cleared */
-
-    if (SW1_Pressed) {                                                              /*  Validación del SW1 */
-        LED_D1_On();
-        LED_D2_Off();
-        LED_D3_Off();
-        LED_D4_Off();
-
-        uint32_t TIMER1_Reload = ReloadStep * 100000;
-        TIMER1_TAILR_R = (TIMER1_Reload & TIMER_TAILR_M);                           /*  GPTM1 => TAILR: GPTM Timer A Interval Load */
-        GPTM1_A_Initiate();                                                         /*  GPTM1 => TAEN: GPTM Timer A Enable -> Enabled and begins counting */
-    }
-
-    if (SW2_Pressed) {                                                              /*  Validación del SW2 */
-        GPTM2_A_Stop();                                                             /*  GPTM2 => TAEN: GPTM Timer A Enable -> Disabled */
-        uint32_t TIMER2_CurrentValue = TIMER2_TAR_R;                                /*  GPTM2 => TAR: Return of the current value */
-        TIMER2_TAV_R = 0;                                                           /*  GPTM2 => TAV: GPTM Timer A Value -> Load the GPTMTAR on the next clock cycle */
-
-        ReactionTime_ms = (float)TIMER2_CurrentValue / 16000;
-
-        if (ReactionTime_ms == 0) {
-            LED_D1_Off();
-            LED_D4_On();
-            NVIC_DIS0_R |= (1 << (21 - 0));                                         /*  Interrupt 21 (TIMER 1 subtimer A) => INT: Interrupt Disable -> INT bit (EN0) cleared */
-            NVIC_DIS0_R |= (1 << (23 - 0));                                         /*  Interrupt 23 (TIMER 2 subtimer A) => INT: Interrupt Disable -> INT bit (EN0) cleared */
-            NVIC_DIS1_R |= (1 << (51 - 32));                                        /*  Interrupt 51 (PortJ) => INT: Interrupt Disable -> INT bit (EN1) cleared */
-        }
-        else if (ReactionTime_ms < 180) {
-            LED_D1_On();
-            LED_D2_On();
-            LED_D3_On();
-            LED_D4_On();
-        }
-        else if (ReactionTime_ms < 220) {
-            LED_D1_On();
-            LED_D2_On();
-            LED_D3_On();
-        }
-        else if (ReactionTime_ms < 260) {
-            LED_D1_On();
-            LED_D2_On();
-        }
-        else if (ReactionTime_ms < 300) {
-            LED_D1_On();
-        }
-    }
-
-    /*  Limpiar la bandera de interrupción (GPIOICR) */
-    GPIO_PORTJ_AHB_ICR_R |= (GPIO_PIN_1 | GPIO_PIN_0);                              /*  PortJ[1:0] => IC: Interrupt Clear -> Interrupt cleared */
 }
